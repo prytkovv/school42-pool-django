c3ef6052abbf706d2aaa0970984c080edc583e2c
Andrew Prytkov <dromadron@yandex.ru>
2021-05-02 21:30:18 +0300

    implement log reading and some beauty fixes

--- d04/d04/ex02/forms.py
+++ d04/d04/ex02/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 
 class TextMessageForm(forms.Form):
-    text_message = forms.CharField(label='Text message', max_length=100)
+    text_message = forms.CharField(
+        label='Please input something',
+        max_length=100)
--- d04/d04/ex02/views.py
+++ d04/d04/ex02/views.py
@@ -6,15 +6,29 @@ import datetime
 from . import forms
 
 
-def write_record(record):
+def create_record(record):
     with open(getattr(settings, 'LOGFILE', None), 'a') as f:
         current_datetime = str(
             datetime.datetime.today().replace(
                 microsecond=0)).split()
         f.write(
-            'Message:%s\nDate:%s\nTime:%s\n'
+            'Message=%s;Date=%s;Time=%s\n'
             % (record, current_datetime[0], current_datetime[1])
         )
+
+
+def get_record_list():
+    try:
+        with open(getattr(settings, 'LOGFILE', None)) as f:
+            return [
+                {
+                    item.split('=')[0]: item.split('=')[1]
+                    for item in record.split(';')
+                }
+                for record in f.readlines()
+            ]
+    except FileNotFoundError:
+        return []
 
 
 def get_text_message(request):
@@ -22,7 +36,10 @@ def get_text_message(request):
         form = forms.TextMessageForm(request.POST)
         if form.is_valid():
             text_message = form.cleaned_data['text_message']
-            write_record(text_message)
+            create_record(text_message)
     else:
         form = forms.TextMessageForm()
-    return render(request, 'ex02/form.html', {'form': form})
+    return render(
+        request, 'ex02/form.html',
+        {'form': form, 'record_list': get_record_list()}
+    )
