75957bccee2ed26d24c1e03b084611809937f565
Andrew Prytkov <dromadron@yandex.ru>
2021-05-14 02:25:35 +0300

    add rating system, change func views to class based and some beauty changes

--- d06/ex/models.py
+++ d06/ex/models.py
@@ -1,10 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Vote(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tip = models.ForeignKey('Tip', on_delete=models.CASCADE)
+    choice = models.CharField(max_length=2)
+
+    class Meta:
+        unique_together = ('author', 'tip',)
+
+    def reverse_choice(self):
+        if self.choice == 'UP':
+            self.choice = 'DN'
+        elif self.choice == 'DN':
+            print('here')
+            self.choice = 'UP'
+        self.save()
+
+    def __str__(self):
+        return self.choice
 
 
 class Tip(models.Model):
 
     content = models.TextField()
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
 
+    def get_tip_rating(self):
+        return self.vote_set.filter(
+            choice='UP').count() - self.vote_set.filter(choice='DN').count()
 
+    def upvote(self, voter):
+        my_tip = self
+        vote = Vote.objects.create(
+            author=voter,
+            tip=my_tip,
+            choice='UP')
+        my_tip.vote_set.add(vote)
+        my_tip.save()
+
+    def downvote(self, voter):
+        my_tip = self
+        vote = Vote.objects.create(
+            author=voter,
+            tip=my_tip,
+            choice='DN')
+        my_tip.vote_set.add(vote)
+        my_tip.save()
+
+    def __str__(self):
+        return self.content
--- d06/ex/views.py
+++ d06/ex/views.py
@@ -1,91 +1,138 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
-from django.http import HttpResponse
 from django.db import IntegrityError
+from django.views.generic.list import ListView
+from django.views.generic import FormView
+from django.views.generic.edit import ModelFormMixin
+
 
 from . import forms
 from . import models
 
 
-def index(request):
-    tips = models.Tip.objects.all()
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = forms.TipForm(request.POST)
-            if form.is_valid():
-                tip = form.save(commit=False)
-                tip.author = str(request.user)
-                tip.save()
+class IndexView(ListView, ModelFormMixin):
+
+    model = models.Tip
+    template_name = 'ex/index.html'
+    context_object_name = 'tips'
+    form_class = forms.TipForm
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        self.form = self.get_form(self.form_class)
+        return ListView.get(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        self.form = self.get_form(self.form_class)
+
+        if self.form.is_valid():
+            self.object = self.form.save(commit=False)
+            self.object.author = self.request.user
+            self.object.save()
+
+        return self.get(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexView, self).get_context_data(*args, **kwargs)
+        context['form'] = self.form
+        context['username'] = self.request.user
+        return context
+
+
+def upvote_tip(request, tip_id):
+
+    try:
+        tip = models.Tip.objects.get(pk=tip_id)
+        user = request.user
+        tip.upvote(user)
+    except IntegrityError:
+        tip_vote = tip.vote_set.get(author=user)
+        if str(tip_vote) == 'UP':
+            tip_vote.delete()
         else:
-            form = forms.TipForm()
-        return render(request,
-                      'ex/index.html',
-                      {'tips': tips,
-                       'username': request.user,
-                       'form': form})
-    else:
-        return render(request,
-                      'ex/index.html',
-                      {'username': request.user})
+            tip_vote.reverse_choice()
+    finally:
+        return redirect('index')
 
 
-def login(request):
-    if request.user.is_authenticated:
-        return redirect(index)
-    else:
-        if request.method == 'POST':
-            form = forms.LoginForm(request.POST)
-            if form.is_valid():
-                data = form.cleaned_data
-                user = auth.authenticate(
-                    username=data.get('username'),
-                    password=data.get('password'))
-                if user:
-                    if user.is_active:
-                        auth.login(request, user)
-                        return redirect(index)
-                else:
-                    messages.error(request,
-                                   'Username or password is not correct')
-                    return render(request,
-                                  'ex/login.html',
-                                  {'form': form,
-                                   'form.username': data.get('username'),
-                                   'username': request.user})
+def downvote_tip(request, tip_id):
+
+    try:
+        tip = models.Tip.objects.get(pk=tip_id)
+        user = request.user
+        tip.downvote(user)
+    except IntegrityError:
+        tip_vote = tip.vote_set.get(author=user)
+        if str(tip_vote) == 'DN':
+            tip_vote.delete()
         else:
-            form = forms.LoginForm()
-    return render(request,
-                  'ex/login.html',
-                  {'form': form,
-                   'username': request.user})
+            tip_vote.reverse_choice()
+    finally:
+        return redirect('index')
 
 
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect(index)
-    else:
-        if request.method == 'POST':
-            form = forms.SignupForm(request.POST)
-            if form.is_valid():
-                try:
-                    user = User.objects.create_user(
-                        username=form.cleaned_data.get('username'),
-                        password=form.cleaned_data.get('password'))
-                    user.save()
-                    auth.login(request, user)
-                except IntegrityError:
-                    messages.error(request, 'Username is not available')
-                return redirect(index)
+def delete_tip(request, tip_id):
+
+    tip = models.Tip.objects.get(pk=tip_id)
+    tip.delete()
+    return redirect('index')
+
+
+class LoginView(FormView):
+
+    template_name = 'ex/login.html'
+    form_class = forms.LoginForm
+    success_url = '.'
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        context['username'] = self.request.user
+        return context
+
+    def form_valid(self, form):
+        context = form.cleaned_data
+        user = auth.authenticate(
+            username=context.get('username'),
+            password=context.get('password'))
+        if user:
+            if user.is_active:
+                auth.login(self.request, user)
+                return redirect('index')
         else:
-            form = forms.SignupForm()
-    return render(request,
-                  'ex/signup.html',
-                  {'form': form,
-                   'username': request.user})
+            messages.error(self.request,
+                           'Username or password is not correct')
+            return super(LoginView, self).form_valid(form)
+
+
+class SignupView(FormView):
+
+    template_name = 'ex/signup.html'
+    form_class = forms.SignupForm
+    success_url = '.'
+
+    def get_context_data(self, **kwargs):
+        context = super(SignupView, self).get_context_data(**kwargs)
+        context['username'] = self.request.user
+        return context
+
+    def form_valid(self, form):
+        context = form.cleaned_data
+        try:
+            user = User.objects.create_user(
+                username=context.get('username'),
+                password=context.get('password'))
+            user.save()
+            auth.login(self.request, user)
+        except IntegrityError:
+            messages.error(self.request, 'Username is not available')
+            return super(SignupView, self).form_valid(form)
+        else:
+            return redirect('index')
 
 
 def logout(request):
     auth.logout(request)
-    return redirect(index)
+    return redirect('index')
